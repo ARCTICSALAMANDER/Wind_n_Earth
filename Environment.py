791fb40e44e5e6f0c907a20d412e85ce9a8f4602
andrey13tenetko <165918607+andrey13tenetko@users.noreply.github.com>
2026-01-31 23:59:43 +0300

    Кристаллы двери

--- Environment.py
+++ Environment.py
@@ -2,41 +2,55 @@ import arcade
 
 
 class Door(arcade.Sprite):
+
     def __init__(self, image, x, y):
         super().__init__(image, scale=0.5)
         self.center_x = x
         self.center_y = y
-        self.start_x = x
+
+        self.closed_x = x
+        self.is_open = False
+        self.open_offset = 64
 
     def open(self):
-        # дверь уезжает влево
-        self.center_x -= 50
+        if not self.is_open:
+            self.center_x -= self.open_offset
+            self.is_open = True
 
     def close(self):
-        # дверь возвращается обратно
-        self.center_x = self.start_x
+        if self.is_open:
+            self.center_x = self.closed_x
+            self.is_open = False
 
 
 class Button(arcade.Sprite):
+
     def __init__(self, image, x, y, door: Door):
         super().__init__(image, scale=0.5)
         self.center_x = x
         self.center_y = y
 
-        # дверь, которую открывает кнопка
         self.door = door
         self.isPressed = False
 
-    def press(self):
-        self.isPressed = True
-        self.door.open()
+    def update(self, characters):
+        pressed_now = False
 
-    def release(self):
-        self.isPressed = False
-        self.door.close()
+        for character in characters:
+            if arcade.check_for_collision(self, character):
+                pressed_now = True
+                break
+
+        if pressed_now and not self.isPressed:
+            self.isPressed = True
+            self.door.open()
+        elif not pressed_now and self.isPressed:
+            self.isPressed = False
+            self.door.close()
 
 
 class Crystal(arcade.Sprite):
+
     def __init__(self, image, x, y, game, character):
         super().__init__(image, scale=0.5)
         self.center_x = x
@@ -44,8 +58,16 @@ class Crystal(arcade.Sprite):
 
         self.character = character
         self.game = game
+        self.collected = False
 
-    def collect(self, character):
-        if isinstance(character, self.character):
-            self.game.crystalCount += 1
-            self.remove_from_sprite_lists() 
+    def update(self, characters):
+        if self.collected:
+            return
+
+        for char in characters:
+            if arcade.check_for_collision(self, char):
+                if isinstance(char, self.character):
+                    self.collected = True
+                    self.game.crystalCount += 1
+                    self.remove_from_sprite_lists()
+                    break
