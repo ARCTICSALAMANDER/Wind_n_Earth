d7070305ac30d46c5e4beb3c2d825369df27cdbc
andrey13tenetko <165918607+andrey13tenetko@users.noreply.github.com>
2026-01-31 21:55:14 +0300

    7

--- Environment.py
+++ Environment.py
@@ -1,61 +1,51 @@
-# НЕ ИМПОРТИРОВАТЬ КЛАССЫ ИЗ ДРУГИХ СКРИПТОВ ПРОЕКТА
 import arcade
 
 
 class Door(arcade.Sprite):
-    '''Класс двери'''
-
-    def __init__(self, image, scale, x, y, doorId):
-        super().__init__(image, scale)
-        self.doorId = doorId # номер, по которому связываем кнопку с дверью
+    def __init__(self, image, x, y):
+        super().__init__(image, scale=0.5)
+        self.center_x = x
+        self.center_y = y
+        self.start_x = x
 
     def open(self):
-        '''Метод открытия двери'''
-        # чтобы было проще с анимацией, делаем пока что
-        # только горизонтальные двери, которые по нажатию
-        # кнопки будут уезжать внутрь стены
-        ...
+        # дверь уезжает влево
+        self.center_x -= 50
 
     def close(self):
-        '''Метод закрытия двери'''
-        # дверь выезжает из стены
+        # дверь возвращается обратно
+        self.center_x = self.start_x
 
 
 class Button(arcade.Sprite):
-    '''Класс кнопки'''
+    def __init__(self, image, x, y, door: Door):
+        super().__init__(image, scale=0.5)
+        self.center_x = x
+        self.center_y = y
 
-    def __init__(self, image, scale, x, y, door: Door):
-        super().__init__(image, scale)
-        self.position = (x, y)
-        # сюда кладете дверь, которая будет открываться
-        # по нажатию этой кнопки, при коллизии любого
-        # персонажа и этой кнопки вызвать self.door.open()
+        # дверь, которую открывает кнопка
         self.door = door
-
-        # нажата ли кнопка. Если персонаж наступит, 
-        # self.isPressed = True, если уйдет с кнопки - 
-        # self.isPressed = False и self.door.close()
         self.isPressed = False
+
+    def press(self):
+        self.isPressed = True
+        self.door.open()
+
+    def release(self):
+        self.isPressed = False
+        self.door.close()
 
 
 class Crystal(arcade.Sprite):
-    '''Класс кристалла'''
+    def __init__(self, image, x, y, game, character):
+        super().__init__(image, scale=0.5)
+        self.center_x = x
+        self.center_y = y
 
-    def __init__(self, image, scale, x, y, game, character):
-        super().__init__(image, scale)
-        self.position = (x, y)
-        # сюда кладете строку с именем персонажа, который должен
-        # этот кристалл собрать ("Wind" / "Earth"). При коллизии
-        # проверяте, что атрибут name у персонажа равен self.character,
-        # затем делаем self.kill(), чтобы удалить кристалл
         self.character = character
-
-        # когда кристалл соберется нужным персонажем, нужно сделать
-        # self.game.crystalCount += 1
         self.game = game
 
-
-class Wall(arcade.Sprite):
-    def __init__(self, image, scale, x, y):
-        super().__init__(image, scale)
-        self.position = (x, y)
+    def collect(self, character):
+        if isinstance(character, self.character):
+            self.game.crystalCount += 1
+            self.remove_from_sprite_lists()
